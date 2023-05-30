3157157261553609a84381cbc7006a62438824f0
47nick <paul_nick@web.de>
2023-05-30 13:27:51 +0200

    updated coneptual model

--- Conceptual_Model.R
+++ Conceptual_Model.R
@@ -5,41 +5,73 @@ library(DiagrammeR)
 
 
 
+
 DiagrammeR('graph LR
-           B(overall_benefits)-->A(result)
-           C(benefits_agroforestry)-->B
+           B(Overall benefits)-->A(result)
+           B2(Type of panel)-->G
+           C((Benefits agroforestry))-->B
            I(Ecosystem Service)-->C
-           A2(Shading Effect)-->C
+           A2(Abiotic factors)-->D
+           A5(Transpirationrate)-->A2
+           A4(PAR Radiation)-->A2
            J(Subsidies)-->C
            N(Investment Cost)-->C
            T(Hired Labour)-->N
-           U(Seed)-->N
+           U(Transplants)-->N
            V(Machinery)-->N
            W(Plant protection)-->N
            X(Manures and fertilizers)-->N
            O(Annual cost)-->P
-           S(Maintenance Cut)-->O
-           P(Profit Margin)-->C
-           Q(Margin Wood)-->P
-           R(Harvesting Cost)-->P
-           D(benefits_agripv)-->B
-           A3(Shading Effect)-->D
+           S(Maintenance cut)-->O
+           P(Profit margin)-->C
+           Q(Wood yield)-->P
+           R(Harvesting cost)-->P
+           D((Benefits agripv))-->B
            J2(Subsidies)-->D
-           Y(Investment Cost)-->Z
-           G(Cost of Panels)-->Y
-           H(Hired Labour)-->Y
-           Z(Profit Margin)-->D
-           E(Profit through Energy )-->Z
+           Y(Investment cost)-->Z
+           G(Cost of panels)-->Y
+           H(Hired labour)-->G
+           Z(Profit margin)-->D
+           E(Profit through energy )-->Z
            F(Annual cost)-->Z
-           K(benefits_crops)-->B
-           L(costs_more_cultivation)-->K
-           M(income_more_cultivation)-->K
-           varA(AV variables)
-           varB(Standard farm variables)
+           K((Benefits crops))-->B
+           M(Conventional yield)-->K
+           L1(Yield loss)-->P
+           L2(Yield loss)-->Z
+           P1(Extreme weather event)-->C
+           P1(Extreme weather event)-->D
+           P1(Extreme weather event)-->K
+           H1(High price fluctation)-->C
+           varB(costs)
            varC(Risks)
-           varD(not included yet)
-           style PVA fill:orange, stroke:#333,stroke-width:2px;
-           style varA fill:orange, stroke:#333,stroke-width:4px;
-           style varB stroke:#333,stroke-width:4px;
-           style varC fill:gray, stroke:#333,stroke-width:4px;
-           style varD fill:green, stroke:#333,stroke-width:4px;')
+           varD(profits)
+           style P1  fill:yellow, stroke:#333,stroke-width:2px;
+           style H1  fill:yellow, stroke:#333,stroke-width:2px;
+           style I  fill:lightgreen, stroke:#333,stroke-width:2px;
+           style J  fill:lightgreen, stroke:#333,stroke-width:2px;
+           style Q  fill:lightgreen, stroke:#333,stroke-width:2px;
+           style J2  fill:lightgreen, stroke:#333,stroke-width:2px;
+           style E  fill:lightgreen, stroke:#333,stroke-width:2px;
+           style M  fill:lightgreen, stroke:#333,stroke-width:2px;
+           style B2  fill:lightcoral, stroke:#333,stroke-width:2px;
+           style T  fill:lightcoral, stroke:#333,stroke-width:2px;
+           style U  fill:lightcoral, stroke:#333,stroke-width:2px;
+           style V  fill:lightcoral, stroke:#333,stroke-width:2px;
+           style X  fill:lightcoral, stroke:#333,stroke-width:2px;
+           style S  fill:lightcoral, stroke:#333,stroke-width:2px;
+           style R  fill:lightcoral, stroke:#333,stroke-width:2px;
+           style G  fill:lightcoral, stroke:#333,stroke-width:2px;
+           style H  fill:lightcoral, stroke:#333,stroke-width:2px;
+           style L1  fill:lightcoral, stroke:#333,stroke-width:2px;
+           style L2 fill:lightcoral, stroke:#333,stroke-width:2px;
+           style F  fill:lightcoral, stroke:#333,stroke-width:2px;
+           style O  fill:lightcoral, stroke:#333,stroke-width:2px;
+           style W  fill:lightcoral, stroke:#333,stroke-width:2px;
+           style N  fill:lightcoral, stroke:#333,stroke-width:2px;
+           style Y  fill:lightcoral, stroke:#333,stroke-width:2px;
+           style varB fill:lightcoral, stroke:#333,stroke-width:4px;
+           style varC fill:yellow, stroke:#333,stroke-width:4px;
+           style varD fill:lightgreen, stroke:#333,stroke-width:4px;')
+
+
+
