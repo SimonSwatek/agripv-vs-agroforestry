7c46a8ce463f1c17bd4d921e47caa151e76c9b22
s7rk <robin.kurth24@gmail.com>
2023-06-08 13:16:06 +0200

    Build function-decision_model

--- Simulation.R
+++ Simulation.R
@@ -8,8 +8,8 @@ library(readr)
 input_table <- read.csv2("input_estimates.csv", header = TRUE)
 input_table
 
-#Build function###
-
+#Build function-decision_model###
+decision_model <- function(x, varnames){
 #area distribution
 
 area_crop <- 0.8         #area for crops
@@ -22,7 +22,7 @@ area_row <- 1-area_crop  #area of the rows of pv or forest
 income_crop <- 
   crop_yield*crop_monetary
 income_pv <-
-  (income_crop * area_crop * yield_loss_pv) + profit_pv * area_row + subsidies_pv
+  (income_crop * area_crop * yield_loss_pv) + (profit_pv * area_row * fluctuation_pv) + subsidies_pv
 income_af <-
   ( income_crop * area_crop * yield_loss_af) 
   + (profit_af * area_row * fluctuation_af) + subsidies_af + ecosystem_service_af
@@ -32,5 +32,18 @@ income_af <-
 cost_pv <- annual_cost_pv + investment_cost_pv 
 cost_af <- harvesting_cost_af + annual_cost_af + investment_cost_af
 
+#profits
 
+profits_crop <- income_crop
+profits_pv   <- income_pv - cost_pv
+profits_af <- income_af - cost_af
+
+return(profits_crop, profits_pv, profits_af) 
+}
+
+###from seminar 6
+mcSimulation(estimate = as.estimate(input_estimates),
+             model_function = decision_model,
+             numberOfModelRuns = 100,
+             functionSyntax = "plainNames")
   
